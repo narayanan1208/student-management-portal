67be23d56264cb41bd4618553665f1f15e4d201a
narayanansrikumar <nsrikumar@aghedgedesk.com>
2025-01-25 15:41:11 +0530

    Updated frontend for no student list data

--- frontend/src/components/pages/Manage.tsx
+++ frontend/src/components/pages/Manage.tsx
@@ -2,10 +2,7 @@ import { MouseEvent, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Table } from "react-bootstrap";
 import { Button, ButtonToolbar } from "react-bootstrap";
-import {
-  getStudents,
-  deleteStudent,
-} from "../redux-toolkit/StudentService";
+import { getStudents, deleteStudent } from "../redux-toolkit/StudentService";
 import { RootState, AppDispatch } from "../../../store";
 import {
   setAddModalShowTrue,
@@ -33,7 +30,7 @@ const Manage = () => {
 
   const handleUpdate = (e: MouseEvent<HTMLButtonElement>, student: Student) => {
     e.preventDefault();
-    dispatch(setUpdatedStudent(student))
+    dispatch(setUpdatedStudent(student));
     dispatch(setEditModalShowTrue());
   };
 
@@ -58,56 +55,58 @@ const Manage = () => {
     <div className="container-fluid side-container">
       <div className="row side-row">
         <p id="before-table"></p>
-        <Table
-          striped
-          bordered
-          hover
-          className="react-bootstrap-table"
-          id="dataTable"
-        >
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Registration No</th>
-              <th>Email</th>
-              <th>Course</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((stu) => (
-              <tr key={stu.studentId}>
-                <td>{stu.studentId}</td>
-                <td>{stu.firstName}</td>
-                <td>{stu.lastName}</td>
-                <td>{stu.registrationNo}</td>
-                <td>{stu.email}</td>
-                <td>{stu.course}</td>
-                <td>
-                  <Button
-                    className="mr-2"
-                    variant="primary"
-                    onClick={(event) => handleUpdate(event, stu)}
-                  >
-                    Update
-                  </Button>{" "}
-                  <Button
-                    className="mr-2"
-                    variant="danger"
-                    onClick={(event) =>
-                      stu.studentId && handleDelete(event, stu.studentId)
-                    }
-                  >
-                    Delete
-                  </Button>
-                  <UpdateStudentModal />
-                </td>
+        {students?.length > 0 && (
+          <Table
+            striped
+            bordered
+            hover
+            className="react-bootstrap-table"
+            id="dataTable"
+          >
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Registration No</th>
+                <th>Email</th>
+                <th>Course</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {students.map((stu) => (
+                <tr key={stu.studentId}>
+                  <td>{stu.studentId}</td>
+                  <td>{stu.firstName}</td>
+                  <td>{stu.lastName}</td>
+                  <td>{stu.registrationNo}</td>
+                  <td>{stu.email}</td>
+                  <td>{stu.course}</td>
+                  <td>
+                    <Button
+                      className="mr-2"
+                      variant="primary"
+                      onClick={(event) => handleUpdate(event, stu)}
+                    >
+                      Update
+                    </Button>{" "}
+                    <Button
+                      className="mr-2"
+                      variant="danger"
+                      onClick={(event) =>
+                        stu.studentId && handleDelete(event, stu.studentId)
+                      }
+                    >
+                      Delete
+                    </Button>
+                    <UpdateStudentModal />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
         <ButtonToolbar>
           <Button variant="primary" onClick={(event) => handleAdd(event)}>
             Add Student
--- frontend/src/components/pages/Students.tsx
+++ frontend/src/components/pages/Students.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Table } from "react-bootstrap";
 import { getStudents } from "../redux-toolkit/StudentService";
 import { RootState, AppDispatch } from "../../../store";
 import "../../App.css";
 
 const Students = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const { students, status } = useSelector(
     (state: RootState) => state.students
@@ -21,36 +23,55 @@ const Students = () => {
     <div className="container-fluid side-container">
       <div className="row side-row">
         <p id="before-table"></p>
-        <Table
-          striped
-          bordered
-          hover
-          className="react-bootstrap-table"
-          id="dataTable"
-        >
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Registration No</th>
-              <th>Email</th>
-              <th>Course</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((stu) => (
-              <tr key={stu.studentId}>
-                <td>{stu.studentId}</td>
-                <td>{stu.firstName}</td>
-                <td>{stu.lastName}</td>
-                <td>{stu.registrationNo}</td>
-                <td>{stu.email}</td>
-                <td>{stu.course}</td>
+        {students?.length > 0 && (
+          <Table
+            striped
+            bordered
+            hover
+            className="react-bootstrap-table"
+            id="dataTable"
+          >
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Registration No</th>
+                <th>Email</th>
+                <th>Course</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {students.map((stu) => (
+                <tr key={stu.studentId}>
+                  <td>{stu.studentId}</td>
+                  <td>{stu.firstName}</td>
+                  <td>{stu.lastName}</td>
+                  <td>{stu.registrationNo}</td>
+                  <td>{stu.email}</td>
+                  <td>{stu.course}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+        {students?.length === 0 && (
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h2>
+              No student list to display.{' '}
+              <span
+                style={{
+                  color: "blue",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                onClick={() => navigate("/manage")}
+              >
+                Click here to add students.
+              </span>
+            </h2>
+          </div>
+        )}
       </div>
     </div>
   );
